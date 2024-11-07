53931ca02aa415d97f8f6533a2adfc079813cb1b
frede791 <frederik.anilmarkus@gmail.com>
2024-11-07 15:26:02 -0500

    some command line improvements
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- dummy_example.py
+++ dummy_example.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
 
 class beacon:
     def __init__(self, x, y):
@@ -27,25 +26,33 @@ class beacon:
 
 class robot:
 
-    def __init__(self):
+    def __init__(self, Q_weight):
         self.pos = np.array([[0],[1]])
-        self.cov = 1*np.eye(2)
+        self.cov = Q_weight*np.eye(2)
 
-        
-N = 1000 # Number of timesteps
+
+# Parameters
+N = 1000  # Number of timesteps
+n_beacons = 6 # Number of beacons
+Q_weight = 0.25
+R_weight = 0.25
 
 # Init 
 beacons = []
-n_beacons = 3 # Number of beacons
 beacon1 = beacon(0,0)
 beacon2 = beacon(1,1)
-beacons = [beacon1, beacon2]
-# beacon3 = beacon(0,5)
-# beacons = [beacon1, beacon2, beacon3]
-robot = robot()
+# beacons = [beacon1, beacon2]
+beacon3 = beacon(0,2)   
+beacon4 = beacon(-1,1)
+beacon5 = beacon(-1,2)
+beacon6 = beacon(1,2)
+beacons = [beacon1, beacon2, beacon3, beacon4, beacon5, beacon6]
 
-Q = 0.25*np.eye(2) # Process noise covariance
-R = 0.25*np.eye(n_beacons) # Measurement noise covariance
+
+robot = robot(Q_weight)
+
+Q = Q_weight*np.eye(2) # Process noise covariance
+R = R_weight*np.eye(n_beacons) # Measurement noise covariance
 K = np.zeros((2,n_beacons)) # Kalman gain
 H = np.zeros((n_beacons,2)) # Measurement Jacobian
 z = np.zeros((n_beacons,1)) # Measurement vector
@@ -60,7 +67,7 @@ positions = np.zeros((N,2))
 
 
 # Dynamics of robot
-A = np.array([[0,1],[1,0]])
+A = np.array([[0,1],[1,0]]) # Simple back and forth motion between (0,1) and (1,0)
 
 i = 0
 for i in range(N):
@@ -73,47 +80,60 @@ for i in range(N):
     info_matrix_p = np.linalg.inv(robot.cov)
     info_vector_p = info_matrix_p@robot.pos
 
-    print(info_vector_p)
+    # print(np.linalg.norm(info_vector_p))
 
     # Measurement update & calculate the information matrix and vector for the measurement
     for j,beacon in enumerate(beacons):
         H[j] = beacon.H(robot.pos)
-        z[j] = beacon.ground_truth_d(i) + np.random.normal(0,R[j,j],1) # Based on the ground truth position of the robot (no uncertainty in positional movement)
-    
+        z[j] = beacon.ground_truth_d(i) + np.random.normal(0,R[j,j],1) # Based on the ground truth position of the robot with some additional noise
         actual_pos[j] = beacon.h(robot.pos) # Based on where the measurement model believes the robot is
 
     K = robot.cov@H.T@np.linalg.inv(H@robot.cov@H.T + R)
     robot.pos = robot.pos + K@(z - actual_pos)
     robot.cov = (np.eye(2) - K@H)@robot.cov
 
+
     # Update the information matrix and vector
-    I_matrix = H.T@np.linalg.inv(R)@H
+    I_matrix = H.T@np.linalg.inv(R)@H   
     I_vector = H.T@np.linalg.inv(R)@z
     
     info_matrix = info_matrix_p + I_matrix
     info_vector = info_vector_p + I_vector
 
-    # Store the information vector and position vector
+    print("-------------------------------------")
+    print("Robot position: ",robot.pos.T)
+    print("Measurement Jacobian: \n", H)
+    print("Distance measurements: \n",z)
+    print("Info vector: ",np.sum(np.absolute(info_vector)))
 
+    # Store the information vector and position vector
     info_vectors[i] = info_vector.T
     positions[i] = robot.pos.T
-    
+
 
 #Plotting
 for i in range (N):
-    plt.plot(positions[i,0],positions[i,1],'ro')
-plt.axis([-2,2,-2,2])
+    if i == 0:
+        plt.plot(positions[i,0],positions[i,1],'ro', label='Robot positions')
+    else:
+        plt.plot(positions[i,0],positions[i,1],'ro')
+
+for i in range(n_beacons):
+    if i == 0:
+        plt.plot(beacons[i].x,beacons[i].y,'yx',label='Beacon')
+    else:
+        plt.plot(beacons[i].x,beacons[i].y,'yx')
+plt.axis([-4,4,-4,4])
 plt.title('Robot position')
+plt.legend()
 
 
 plt.figure()
 plt.plot(info_vectors[:, 0], info_vectors[:, 1], marker='x', color='blue', linestyle='-', markersize=5)
-
 plt.title('Information vector')
 
 
 sum_info = np.sum(np.absolute(info_vectors), axis=1)
-
 plt.figure()
 plt.plot(sum_info, marker='x', color='green', linestyle='-', markersize=1)
 plt.title('sum of dimensions of information vector')
