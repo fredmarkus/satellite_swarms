af936e38428dcffc64d29094218e059f0296a2a9
frede791 <frederik.anilmarkus@gmail.com>
2024-11-11 10:11:36 -0500

    working implementation (semantically still broken it seems)
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- combined_measurement.py
+++ combined_measurement.py
@@ -8,48 +8,46 @@ import numpy as np
 
 class landmark:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.pos = np.array([x,y])
 
 
 class robot:
 
     def __init__(self, position, rob_cov_init, robot_id):
         self.pos_m = np.array([position[0],position[1]])
-        self.cov_p = rob_cov_init*np.eye(2)
+        self.cov_m = rob_cov_init*np.eye(2)
         self.actual_pos = np.array([position[0],position[1]])
         self.A = np.array([[0,1],[1,0]])
-        self.info_matrix = np.linalg.inv(self.cov_p)
-        self.info_vector = self.info_matrix@self.pos
+        self.info_matrix = np.linalg.inv(self.cov_m)
+        self.info_vector = self.info_matrix@self.pos_m
         self.pos_p = self.pos_m # Initialize the prior vector exactly the same as the measurement vector
         self.cov_p = self.cov_m # Initialize the prior covariance exactly the same as the measurement covariance
         self.id = robot_id # Unique identifier for the satellite
 
 
     def h_bearing(self, landmark):
-        return (self.pos_p - np.array([[landmark.x],[landmark.y]]))/np.linalg.norm(self.pos_p - np.array([[landmark.x],[landmark.y]])) # This provides a normalized vector (the bearing)
+        return (self.pos_p - landmark.pos)/np.linalg.norm(self.pos_p - landmark.pos) # This provides a normalized vector (the bearing)
 
     def H_bearing(self, landmark):
-        coeff = 1/np.linalg.norm(self.pos_p - np.array([[landmark.x],[landmark.y]]))
-        H11 = ((self.pos_p[0][0] - landmark.x)**2)/(np.linalg.norm(self.pos_p - np.array([[landmark.x],[landmark.y]])))**6 + 1
-        H12 = (self.pos_p[0][0] - landmark.x)*(self.pos_p[1][0] - landmark.y)/(np.linalg.norm(self.pos_p - np.array([[landmark.x],[landmark.y]]))**6)
-        H22 = ((self.pos_p[1][0] - landmark.y)**2)/(np.linalg.norm(self.pos_p - np.array([[landmark.x],[landmark.y]])))**6 + 1
+        coeff = 1/np.linalg.norm(self.pos_p - landmark.pos)
+        H11 = ((self.pos_p[0] - landmark.pos[0])**2)/(np.linalg.norm(self.pos_p - landmark.pos))**6 + 1
+        H12 = (self.pos_p[0] - landmark.pos[0])*(self.pos_p[1] - landmark.pos[1])/(np.linalg.norm(self.pos_p - landmark.pos)**6)
+        H22 = ((self.pos_p[1] - landmark.pos[1])**2)/(np.linalg.norm(self.pos_p - landmark.pos))**6 + 1
         return coeff*np.array([[H11, H12],[H12, H22]])
     
     def h_inter_range(self, sat_pos):
-        return np.linalg.norm(np.array([self.pos_p[0][0] - sat_pos[0], self.pos_p[1][0] - sat_pos[1]]))
+        return np.array([np.linalg.norm(self.pos_p - sat_pos)])
     
     def H_inter_range(self, sat_pos):
-        dx = (sat_pos[0] - self.pos_p[0][0])/(np.linalg.norm(np.array([sat_pos[0] - self.pos_p[0][0], sat_pos[1] - self.pos_p[1][0]]))) 
-        dy = (sat_pos[1] - self.pos_p[1][0])/(np.linalg.norm(np.array([sat_pos[0] - self.pos_p[0][0], sat_pos[1] - self.pos_p[1][0]])))
+        dx = (sat_pos[0] - self.pos_p[0])/np.linalg.norm(self.pos_p - sat_pos)
+        dy = (sat_pos[1] - self.pos_p[1])/np.linalg.norm(self.pos_p - sat_pos)
         return np.array([[dx, dy]])
     
     def h_combined(self, landmark, sats):
-        h = np.array()
-        h = np.vstack(h, self.h_bearing(landmark))
+        h = self.h_bearing(landmark)
         for sat in sats:
             if sat.id != self.id:
-                h = np.vstack(h, self.h_inter_range(sat.pos_p))
+                h = np.append(h, self.h_inter_range(sat.pos_p),axis=0)
 
         return h
 
@@ -63,25 +61,24 @@ class robot:
         return H
     
     def true_new_pos(self, Q_weight):
-        self.actual_pos = self.A@self.actual_pos + np.random.normal(loc=0,scale=Q_weight,size=(2,1))
+        self.actual_pos = self.A@self.actual_pos + np.random.normal(loc=0,scale=Q_weight,size=(2))
 
 
     def measure_z_landmark(self, landmark):
-        return np.array([[self.actual_pos[0][0]-landmark.x],[self.actual_pos[1][0]-landmark.y]]/
-                                np.linalg.norm(np.array([[self.actual_pos[0][0]-landmark.x],[self.actual_pos[1][0]-landmark.y]])))
+        return (self.actual_pos -landmark.pos)/np.linalg.norm(self.actual_pos - landmark.pos)
+        
 
-    def measure_z_bearing(self, sats):
-        z = np.array()
+    def measure_z_range(self, sats):
+        z = np.empty((0))
         for sat in sats:
             if sat.id != self.id:
-                z = np.vstack(z, np.array([[np.linalg.norm(np.array([self.actual_pos[0][0]-sat.actual_pos[0], self.actual_pos[1][0]-sat.actual_pos[1]]))]]))
+                z = np.append(z,np.array([np.linalg.norm(self.actual_pos - sat.actual_pos)]),axis=0)
 
         return z
     
     def measure_z_combined(self, landmark, sats):
-        z = np.array()
-        z = np.vstack(z, self.measure_z_bearing(sats))
-        z = np.vstack(z, self.measure_z_landmark(landmark))
+        z = self.measure_z_landmark(landmark) # Take bearing measurement
+        z = np.append(z, self.measure_z_range(sats),axis=0) # Append the range measurement
 
         return z
 
@@ -89,11 +86,12 @@ class robot:
 # Parameters
 N = 1000  # Number of timesteps
 n_landmarks = 1 # Number of landmarks
-n_satellites = 2 # Number of satellites
-Q_weight = 0.5
-R_weight = 0.25
-rob_cov_init = 0.25
+n_satellites = 1 # Number of satellites
+Q_weight = 0.1
+R_weight = 0.1
+rob_cov_init = 0.1
 dim = 2 #dimension of the space
+meas_dim = dim + n_satellites # dimension is 1 bearing measurement that has dimension dim and n_satellites range measurements each with dimension 1 
 
 
 # Init 
@@ -104,7 +102,7 @@ sat2 = robot([3,0], rob_cov_init, 1)
 sats = [sat1, sat2]
 
 Q = Q_weight*np.eye(dim) # Process noise covariance
-R = R_weight*np.eye(dim) # Measurement noise covariance
+R = R_weight*np.eye(meas_dim) # Measurement noise covariance
 
 
 for i in range(N):
@@ -135,6 +133,9 @@ for i in range(N):
 
         sat.info_matrix = sat.info_matrix + I_matrix
         sat.info_vector = sat.info_vector + I_vector
+
+    # print(sat1.info_matrix)
+    print(sat1.actual_pos)
 
 
         #TODO: Implement plotting
