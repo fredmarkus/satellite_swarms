250d640ff4e3d758a41ddc83336dfe8095f8efe8
frede791 <frederik.anilmarkus@gmail.com>
2024-12-01 11:52:57 -0500

    add elliptical orbits and reorder utils
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

---
+++ utils/math_utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def R_X(theta):
+    return np.array([
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta), np.cos(theta)]
+    ])
+
+def R_Y(theta):
+    return np.array([
+        [np.cos(theta), 0, np.sin(theta)],
+        [0, 1, 0],
+        [-np.sin(theta), 0, np.cos(theta)]
+    ])
+
+def R_Z(theta):
+    return np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+    ])
+
+# Angle must be in radians
+def normalize_angle(angle):
+    return (angle + np.pi) % (2 * np.pi) - np.pi
