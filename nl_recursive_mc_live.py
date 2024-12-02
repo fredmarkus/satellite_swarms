ac1cc82f27f66e3f3b0806d231e4aa60842baeb4
frede791 <frederik.anilmarkus@gmail.com>
2024-12-02 12:23:11 -0500

    add improvements to live script
    Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

--- nl_recursive_mc_live.py
+++ nl_recursive_mc_live.py
@@ -14,13 +14,28 @@ import threading
 import time
 from collections import deque
 
+from utils.math_utils import R_X, R_Z
+
+
 # from functools import partial
 
 
 # Constants
-MU = 3.986004418 * 10**5 # km^3/s^2
-R_EARTH = 6378
-R_SAT = 550 + R_EARTH
+MU = 3.986004418 * 10**5 # km^3/s^2 # Gravitational parameter of the Earth
+R_EARTH = 6378 # km # Radius of the earth
+HEIGHT = 550 # km # Height of the satellite
+R_SAT = HEIGHT + R_EARTH
+ROH_0 = 1.225e9 # kg/km^3 # Density of air at sea level
+H_0 = 0 # km # Height of sea level
+MASS = 2 # kg # Mass of the satellite
+AREA = 1e-9 # km^2 # Cross-sectional area of the satellite
+SCALE_HEIGHT = 8.4 # km # Scale height of the atmosphere
+C_D = 2.2 # Drag coefficient of the satellite
+EQ_RADIUS = 6378.137 # km # Equatorial radius of the Earth
+POLAR_RADIUS = 6356.7523 # km # Polar radius of the Earth
+J2 = 1.08263e-3 # J2 perturbation coefficient
+
+DENSITY = ROH_0*np.exp(-(HEIGHT-H_0)/SCALE_HEIGHT) # kg/km^3 # Density of the atmosphere at the height of the satellite
 
 class landmark: # Class for the landmark object. Coordinates in ECEF (TODO: Check with Paulo or Zac if this is correct)
     def __init__(self, x: float, y: float, z: float, name: str) -> None:
@@ -31,7 +46,6 @@ class landmark: # Class for the landmark object. Coordinates in ECEF (TODO: Chec
 class satellite:
 
     def __init__(self, 
-                 state: np.ndarray, 
                  pos_cov_init: float, 
                  vel_cov_init: float, 
                  robot_id: int, 
@@ -41,9 +55,30 @@ class satellite:
                  N: int, 
                  n_sats: int, 
                  landmarks: object,
-                 inclination: float,
+                 orbital_elements: dict,
                  camera_exists: bool) -> None:
-        self.x_0 = state
+        
+        # Calculate initial state based on orbital elements placing 
+        a = float(orbital_elements["a"])
+        e = float(orbital_elements["e"])
+        i = np.radians(float(orbital_elements["i"]))
+        omega = np.radians(float(orbital_elements["omega"]))
+        omega_dot = np.radians(float(orbital_elements["omega_dot"]))
+        M = np.radians(float(orbital_elements["M"]))
+        
+        # Position calculation
+        r = a*(1-e**2)/(1+e*np.cos(M))
+        perifocal = np.array([r*np.cos(M), r*np.sin(M), 0])
+        Q = R_X(-omega_dot)@R_Z(-omega)@R_X(-i)
+        pos_0 = Q@perifocal
+
+        # Velocity calculation
+        v_x = -math.sqrt(MU/a)*np.sin(M)/(1+e*np.cos(M))
+        v_y = math.sqrt(MU/a)*(e + np.cos(M))/(1+e*np.cos(M))
+        v_z = 0
+        vel_0 = Q@np.array([v_x, v_y, v_z])
+    
+        self.x_0 = np.append(pos_0,vel_0) # Initial state vector of the satellite
         self.cov_m = np.diag(np.array([float(pos_cov_init), float(pos_cov_init), float(pos_cov_init), float(vel_cov_init), float(vel_cov_init), float(vel_cov_init)]))
         self.id = robot_id # Unique identifier for the satellite
         self.dim = dim # State dimension of the satellite (currently 3 position + 3 velocity)
@@ -52,18 +87,16 @@ class satellite:
         self.N = N # Number of time steps
         self.n_sats = n_sats # Number of satellites
         self.landmarks = landmarks
-        self.inclination = inclination
         self.camera_exists = camera_exists
-        # self.inv_cov = jnp.linalg.inv(self.cov_m)
 
-        #Overwrite the initial state velocities to account for inclination orbital element
-        self.x_0[4] = float(self.x_0[5]*np.cos(np.radians(self.inclination)))
-        self.x_0[5] = float(self.x_0[5]*np.sin(np.radians(self.inclination)))
-        
-        # TODO: Adjust initial x_m with random error direction for both position and velocity (different scales)
-        self.x_m = self.x_0 + np.array([1,0,0,0,0,0]) # Initialize the measurement vector exactly the same as the initial state vector
-        self.x_p = self.x_m # Initialize the prior vector exactly the same as the measurement vector
-        self.cov_p = self.cov_m # Initialize the prior covariance exactly the same as the measurement covariance
+        # Initialize the measurement vector with noise
+        self.x_m = self.x_0# + np.array([1,0,0,0,0,0]) # Initialize the measurement vector exactly the same as the initial state vector
+        x_m_init_noise = np.random.normal(loc=0,scale=math.sqrt(self.R_weight),size=int(self.dim/2))
+        x_m_init_noise = x_m_init_noise/np.linalg.norm(x_m_init_noise) # Normalize the noise vector
+        self.x_m = self.x_m + np.append(x_m_init_noise,np.zeros((3,)),axis=0) # Add the noise to the initial state vector
+
+        self.x_p = self.x_m
+        self.cov_p = self.cov_m # Initialize the prior covariance the same as the measurement covariance
 
         self.min_landmark_list = None # Initialize an array of the closest landmark to the satellite t
         self.curr_pos = self.x_0[0:3] #Determines the current position of the satellite (Necessary for landmark bearing and satellite ranging)
@@ -80,21 +113,17 @@ class satellite:
     def H_landmark(self, x):
         # Use jax to autodifferentiate
         jac = jax.jacobian(self.h_landmark)(x)
-        # min_landmark = closest_landmark(x, self.landmarks)
-        # norm = jnp.sqrt((x[0] - min_landmark[0])**2 + (x[1] - min_landmark[1])**2 + (x[2] - min_landmark[2])**2)
-        # jac = jnp.eye(3)/norm - np.outer(x - min_landmark, x - min_landmark)/(norm**3)
         return jac
 
     def h_inter_range(self, i, j, x): # This function calculates the range measurement between the satellite and another satellite
         sat_id = j-3 # j is the range measurement index starting from 3
         sat_pos = self.other_sats_pos[i,:,sat_id]
-        if self.is_visible(sat_pos):
+        if self.is_visible_ellipse(sat_pos):
             return jnp.linalg.norm(x[0:3] - sat_pos) 
         else:
             return jnp.linalg.norm(0)
 
     def H_inter_range(self, i, j, x):
-        # vec = [i,j,x]
         jac = jax.jacobian(self.h_inter_range, argnums=2)(i, j, x)
         return jac
 
@@ -103,7 +132,7 @@ class satellite:
         for sat in sats:
             if sat.id != self.id:
 
-                if self.is_visible(sat.curr_pos): # If the earth is not in the way, we can measure the range
+                if self.is_visible_ellipse(sat.curr_pos): # If the earth is not in the way, we can measure the range
                     noise = np.random.normal(loc=0,scale=math.sqrt(self.R_weight),size=(1))
                     d = np.array([np.linalg.norm(self.curr_pos - sat.curr_pos)]) + noise
                     z = np.append(z,d,axis=0)
@@ -135,6 +164,28 @@ class satellite:
         # Calculate the angle between the two vectors
         angle = math.acos(round(np.dot(vec,vec_earth)/(np.linalg.norm(vec)*np.linalg.norm(vec_earth)),6)) 
         if abs(angle) < threshold_angle:
+            return False
+        
+        return True
+    
+    def is_visible_ellipse(self, sat_pos) -> bool:
+        # Check if the earth is in the way of the two satellites
+        d = sat_pos - self.curr_pos
+        A = (d[0]**2 + d[1]**2)/(EQ_RADIUS**2) + (d[2]**2)/(POLAR_RADIUS**2)
+        B = 2*(self.curr_pos[0]*d[0] + self.curr_pos[1]*d[1])/(EQ_RADIUS**2) + 2*self.curr_pos[2]*d[2]/(POLAR_RADIUS**2)
+        C = (self.curr_pos[0]**2 + self.curr_pos[1]**2)/(EQ_RADIUS**2) + (self.curr_pos[2]**2)/(POLAR_RADIUS**2)
+        
+        # Calculate the discriminant
+        discriminant = B**2 - 4*A*(C-1)
+        if discriminant < 0:
+            # Solution does not intersect the earth as no real solutions exist
+            return True
+        
+        # Discriminant is positive, calculate the solutions
+        solution1 = (-B + math.sqrt(discriminant))/(2*A)
+        solution2 = (-B - math.sqrt(discriminant))/(2*A)
+        if ((solution1 > 0 or solution2 > 0) and (solution1 < 1 or solution2 < 1)):
+            #One of the solutions is positive and less than 1, the earth is in the way
             return False
         
         return True
@@ -203,11 +254,41 @@ def latlon2ecef(landmarks: list) -> np.ndarray:
     return ecef.reshape(-1,4)
 
 
+def gravitational_acceleration(r):
+    return (-MU / (jnp.linalg.norm(r)**3)) * r
+
+def atmospheric_drag(v):
+    drag = (-0.5*C_D*DENSITY*AREA*v*np.linalg.norm(v)**2)/MASS
+    return drag
+
+def j2_dynamics(r):
+    r_norm = jnp.linalg.norm(r)
+
+    F = 3*MU*J2*EQ_RADIUS**2/(2*r_norm**5)
+    a_x = F*(r[0])*(5*(r[2]/r_norm)**2 - 1)
+    a_y = F*(r[1])*(5*(r[2]/r_norm)**2 - 1)
+    a_z = F*(r[2])*(5*(r[2]/r_norm)**2 - 3)
+
+    return jnp.array([a_x, a_y, a_z])
+
+def j2_jacobian(r):
+    jac = jax.jacobian(j2_dynamics)(r)
+    return jac
+
 def state_transition(x):
     I = np.eye(3)
-    A21 = -MU/(np.linalg.norm(x[0:3])**3)*I + 3*MU*np.outer(x[0:3],x[0:3])/(np.linalg.norm(x[0:3])**5)
-    A = np.block([[np.zeros((3,3)), I], [A21, np.zeros((3,3))]])
+    # Account for j2 dynamics
+    j2_jac = j2_jacobian(x[0:3])
+    
+    # Account for gravitational acceleration
+    grav_jac = -MU/(np.linalg.norm(x[0:3])**3)*I + 3*MU*np.outer(x[0:3],x[0:3])/(np.linalg.norm(x[0:3])**5)
+    
+    # Account for atmospheric drag
+    drag_jac = -DENSITY*C_D*AREA/(2*MASS)*(np.outer(x[3:6],x[3:6])/np.linalg.norm(x[3:6]) + I*np.linalg.norm(x[3:6]))
+    
+    A = np.block([[np.zeros((3,3)), I], [grav_jac+j2_jac, drag_jac]])
     return A
+
 
 def rk4_discretization(x, dt: float):
     r = x[0:3]
@@ -217,25 +298,25 @@ def rk4_discretization(x, dt: float):
         """Derivative of r with respect to time is velocity v."""
         return v
 
-    def dv_dt(r):
+    def dv_dt(r,v):
         """Derivative of v with respect to time is gravitational acceleration."""
-        return (-MU / (jnp.linalg.norm(r)**3)) * r
+        return gravitational_acceleration(r) + atmospheric_drag(v) + j2_dynamics(r)
 
     # Calculate k1 for r and v
     k1_r = dr_dt(v)
-    k1_v = dv_dt(r)
+    k1_v = dv_dt(r,v)
 
     # Calculate k2 for r and v
     k2_r = dr_dt(v + 0.5 * dt * k1_v)
-    k2_v = dv_dt(r + 0.5 * dt * k1_r)
+    k2_v = dv_dt(r + 0.5 * dt * k1_r, v + 0.5 * dt * k1_v)
 
     # Calculate k3 for r and v
     k3_r = dr_dt(v + 0.5 * dt * k2_v)
-    k3_v = dv_dt(r + 0.5 * dt * k2_r)
+    k3_v = dv_dt(r + 0.5 * dt * k2_r, v + 0.5 * dt * k2_v)
 
     # Calculate k4 for r and v
     k4_r = dr_dt(v + dt * k3_v)
-    k4_v = dv_dt(r + dt * k3_r)
+    k4_v = dv_dt(r + dt * k3_r, v + dt * k3_v)
 
     # Combine the k terms to get the next position and velocity
     r_new = r + (dt / 6) * (k1_r + 2 * k2_r + 2 * k3_r + k4_r)
@@ -294,13 +375,13 @@ with open("config.yaml", "r") as file:
 sats = []
 
 for sat_config in config["satellites"]:
-    sat_config["state"] = np.array(sat_config["state"])
-    # sat_config["state"][5]sats_ = float(np.sqrt(MU/R_SAT)) # for now assign velocity using vis-viva equation
+
     sat_config["N"] = N
     sat_config["landmarks"] = landmark_objects
     sat_config["meas_dim"] = meas_dim
     sat_config["n_sats"] = n_sats
     sat_config["R_weight"] = R_weight
+
     satellite_inst = satellite(**sat_config)
     sats.append(satellite_inst)
 
@@ -425,7 +506,7 @@ def data_processing_loop():
                     f_post = f_prior + J.T@R_inv@J
                     fim[start_i:start_i+state_dim,start_i:start_i+state_dim] = f_post
 
-                    print(f"Satellite {sat.id} at time {i} has covariance {sat.cov_m}")
+                    # print(f"Satellite {sat.id} at time {i} has covariance {sat.cov_m}")
 
             if stop_event.is_set():
                 print('Stopping data processing loop')
@@ -521,7 +602,7 @@ ani = animation.FuncAnimation(
     update,
     init_func=init,
     frames=infinite_frames,    # Use the infinite generator
-    save_count=1000,           # Specify a reasonable number of frames to cache
+    save_count=400,           # Specify a reasonable number of frames to cache
     blit=False,
     interval=100,
     repeat=False
