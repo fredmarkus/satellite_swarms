add sat bearing script and fix bug in jacobian matrix creation
Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

nl_recursive_sat_bearing.py
@@ -14,6 +14,7 @@ from tqdm import tqdm
 from analysis import fpost_sanity_check
 from analysis import get_cov_trace
 from analysis import get_crb_trace
+from sat.construct_jacobian import combined_state_H_bearing_range
 from sat.construct_jacobian import combined_state_H_landmark_range
 from sat.dynamics import exchange_trajectories
 from sat.dynamics import rk4_discretization
@@ -155,28 +156,44 @@ def run_simulation(args):
 
             for sat in sats_copy:
 
+                # Take measurements first.
+                y_m_bearing = sat.measure_z_bearing(sats_copy).tolist()
+
+                sat.bearing_dim = int(len(y_m_bearing))
+
+                y_m_range = sat.measure_z_range(sats_copy).tolist()
+
+                y_m_bearing.extend(y_m_range)
+                y_m = y_m_bearing
+
+                meas_dim = int(len(y_m))
+
+                # y_m.extend(sat.measure_z_range(sats_copy).tolist())
+
+
                 # Get visible landmarks
-                visible_landmarks = sat.visible_landmarks_list(sat.x_p)
-                sat.bearing_dim = len(visible_landmarks) * 3
-                meas_dim = n_sats - 1 + sat.bearing_dim
+                # visible_landmarks = sat.visible_landmarks_list(sat.x_p)
+                # sat.bearing_dim = len(visible_landmarks) * 3
+                # meas_dim = n_sats - 1 + sat.bearing_dim
 
                 # Re-initialize the measurement matrices for each satellite with the correct dimensions
                 # based on the number of visible landmarks
-                y_m = np.zeros((meas_dim))
-                h = np.zeros((meas_dim))
+                # y_m = np.zeros((meas_dim))
+                # h = np.zeros((meas_dim))
                 R_vec = np.append(R_vec, [sat.R_weight_bearing] * sat.bearing_dim)
 
-                # Calculate Jacobian matrix H for combined state (still just one satellite H)
-                H = combined_state_H_landmark_range(sat, meas_dim, state_dim, i)
 
                 # Calculate h
-                h[0 : sat.bearing_dim] = sat.h_landmark(sat.x_p[0:3])
-                for j in range(sat.bearing_dim, meas_dim):
-                    h[j] = sat.h_inter_range(i + 1, j, sat.x_p[0:3])
-                    R_vec = np.append(R_vec, sat.R_weight_range)
+                h = []
+                for j in range(0,int(sat.bearing_dim/3)):
+                    h.extend(sat.h_sat_bearing(i + 1, j, sat.x_p[0:3]).tolist())
 
-                y_m[0 : sat.bearing_dim] = sat.measure_z_landmark()
-                y_m[sat.bearing_dim : meas_dim] = sat.measure_z_range(sats_copy)
+                for j in range(sat.bearing_dim, meas_dim):
+                    h.append(sat.h_inter_range(i + 1, j, sat.x_p[0:3]).item())
+                    R_vec = np.append(R_vec, sat.R_weight_range)
+                
+                # Calculate Jacobian matrix H for combined state (still just one satellite H)
+                H = combined_state_H_bearing_range(sat, meas_dim, state_dim, i)
 
                 # Append vectors and matrices to combined form
                 comb_y_m = np.append(comb_y_m, y_m, axis=0)
@@ -185,6 +202,7 @@ def run_simulation(args):
                     comb_H = H
                 else:
                     comb_H = np.append(comb_H, H, axis=0)
+
 
             # Create R based on the number of measurements of all satellites
             R = np.diag(R_vec)

sat/construct_jacobian.py
@@ -8,7 +8,43 @@ import numpy as np
 # pylint: disable=invalid-name
 from sat.core import satellite
 
-def combined_state_H(sat: satellite,  meas_dim: int, state_dim: int, timestep: int, k: int) -> np.ndarray:
+def combined_state_H_landmark_range(sat: satellite,  meas_dim: int, state_dim: int, timestep: int) -> np.ndarray:
+
+    """
+    Calculate the Jacobian matrix for a single satellite using the combined measurement model.
+
+    Args:
+        sat (satellite): The satellite instance.
+        meas_dim (int): The dimension of the measurement vector.
+        state_dim (int): The dimension of the state vector.
+        timestep (int): The current timestep.
+
+    Returns:
+        np.ndarray: The Jacobian matrix for the satellite.
+    """
+
+    H = np.zeros((meas_dim, state_dim * sat.n_sats))
+    H[0 : sat.bearing_dim, sat.id * state_dim : (sat.id + 1) * state_dim] = (
+        sat.H_landmark(sat.x_p)
+    )
+
+    opp_sat_id = 0
+    for j in range(sat.bearing_dim, meas_dim):
+        dist = sat.H_inter_range(timestep + 1, j, sat.x_p)
+        H[j, sat.id * state_dim : (sat.id + 1) * state_dim] = dist
+
+        if opp_sat_id == sat.id:
+            opp_sat_id += 1
+            H[j, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -dist
+            opp_sat_id += 1
+        else:
+            H[j, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -dist
+            opp_sat_id += 1
+
+    return H
+
+
+def combined_state_H_bearing_range(sat: satellite,  meas_dim: int, state_dim: int, timestep: int) -> np.ndarray:
 
     """
     Calculate the Jacobian matrix for a single satellite using the combined measurement model.
@@ -23,11 +59,23 @@ def combined_state_H(sat: satellite,  meas_dim: int, state_dim: int, timestep: i
     Returns:
         np.ndarray: The Jacobian matrix for the satellite.
     """
-
     H = np.zeros((meas_dim, state_dim * sat.n_sats))
-    H[0 : sat.bearing_dim, k * state_dim : (k + 1) * state_dim] = (
-        sat.H_landmark(sat.x_p)
-    )
+
+    opp_sat_id = 0
+    for j in range(0, int(sat.bearing_dim/3)):
+        bearing = sat.H_sat_bearing(timestep + 1, j, sat.x_p)
+        H[j*3 : (j+1)*3, sat.id * state_dim : (sat.id + 1) * state_dim] = (
+            bearing
+        )
+
+        if opp_sat_id == sat.id:
+            opp_sat_id += 1
+            H[j*3:(j+1)*3, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -bearing
+            opp_sat_id += 1
+
+        else:
+            H[j*3:(j+1)*3, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -bearing
+            opp_sat_id += 1
 
     opp_sat_id = 0
     for j in range(sat.bearing_dim, meas_dim):
@@ -37,6 +85,7 @@ def combined_state_H(sat: satellite,  meas_dim: int, state_dim: int, timestep: i
         if opp_sat_id == sat.id:
             opp_sat_id += 1
             H[j, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -dist
+            opp_sat_id += 1
         else:
             H[j, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -dist
             opp_sat_id += 1

sat/core.py
@@ -99,6 +99,9 @@ class satellite:
                 self.curr_visible_landmarks.append(landmark)
 
         return self.curr_visible_landmarks
+    
+    def visible_sats_list(self):
+        pass
 
 
     def h_landmark(self, x):
@@ -119,7 +122,8 @@ class satellite:
         sat_id = j-self.bearing_dim # j is the range measurement index starting from 3
         sat_pos = self.other_sats_pos[timestep,:,sat_id] # sat_id here refers to the first other satellite. Indexing starts again from 0
         if self.is_visible_ellipse(x[0:3], sat_pos) or self.ignore_earth:
-            return jnp.linalg.norm(x[0:3] - sat_pos) 
+            return jnp.linalg.norm(x[0:3] - sat_pos)
+              
         else:
             return jnp.linalg.norm(0)
 
@@ -129,8 +133,8 @@ class satellite:
         return jac
     
     def h_sat_bearing(self, timestep, j, x):
-        sat_id = j-self.bearing_dim # j is the bearing measurement index starting from 3
-        sat_pos = self.other_sats_pos[timestep,:,sat_id] # sat_id here refers to the first other satellite. Indexing starts again from 0
+        # sat_id = j-self.bearing_dim # j is the bearing measurement index starting from 3
+        sat_pos = self.other_sats_pos[timestep,:,j] # sat_id here refers to the first other satellite. Indexing starts again from 0
         if self.is_visible_ellipse(x[0:3], sat_pos) or self.ignore_earth:
             return (x[0:3] - sat_pos)/jnp.linalg.norm(x[0:3] - sat_pos)
         else:
@@ -189,7 +193,16 @@ class satellite:
 
 
     def is_visible_ellipse(self, own_pos, other_pos) -> bool:
-        # Check if the earth is in the way of the own position and the other position
+        """
+        Check if the earth is in the way of the own position and the other position
+
+        Args:
+            own_pos (np.ndarray): The position of the satellite
+            other_pos (np.ndarray): The position of the other satellite
+
+        Returns:
+            bool: True if the earth is not in the way, False otherwise
+        """
         d = other_pos - own_pos
         A = (d[0]**2 + d[1]**2)/(EQ_RADIUS**2) + (d[2]**2)/(POLAR_RADIUS**2)
         B = 2*(own_pos[0]*d[0] + own_pos[1]*d[1])/(EQ_RADIUS**2) + 2*own_pos[2]*d[2]/(POLAR_RADIUS**2)