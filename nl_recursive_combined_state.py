bug fix on Jacobian and measurement function
Signed-off-by: frede791 <frederik.anilmarkus@gmail.com>

nl_recursive_combined_state.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.linalg import block_diag
 from tqdm import tqdm
+import yaml
 
 from analysis import fpost_sanity_check
 from analysis import get_cov_trace
@@ -124,15 +125,14 @@ def run_simulation(args):
         for i in tqdm(range(N), desc="Timesteps"):
 
             for k, sat in enumerate(sats_copy):
-                sat.curr_pos = x_traj[
-                    i + 1, 0:3, sat.id
-                ]  # Provide the underlying groundtruth position to the satellite for bearing and ranging measurements
+                sat.curr_pos = x_traj[i + 1, 0:3, sat.id]  
+
+                # Provide the underlying groundtruth position to the satellite for bearing and ranging measurements
                 sat.x_p = rk4_discretization(sat.x_m, dt)
 
                 # Assign the state transition matrix to the correct block in the A matrix
-                A[
-                    k * state_dim : (k + 1) * state_dim,
-                    k * state_dim : (k + 1) * state_dim,
+                A[k * state_dim : (k + 1) * state_dim,
+                  k * state_dim : (k + 1) * state_dim,
                 ] = state_transition(sat.x_m)
 
                 # Update the combined state vector and underlying groundtruth
@@ -154,35 +154,37 @@ def run_simulation(args):
 
             R_vec = np.array([])  # Combined measurement noise vector
 
-            for k, sat in enumerate(sats_copy):
+            for sat in sats_copy:
 
                 # Get visible landmarks
-                visible_landmarks = sat.visible_landmarks_list(sat.x_p)
+                visible_landmarks = sat.visible_landmarks_list()
+                visible_sats = sat.visible_sats_list(sats_copy)
+
                 sat.bearing_dim = len(visible_landmarks) * 3
-                meas_dim = n_sats - 1 + sat.bearing_dim
+                meas_dim = len(visible_sats) + sat.bearing_dim
 
                 # Re-initialize the measurement matrices for each satellite with the correct dimensions
                 # based on the number of visible landmarks
                 y_m = np.zeros((meas_dim))
                 h = np.zeros((meas_dim))
                 R_vec = np.append(R_vec, [sat.R_weight_bearing] * sat.bearing_dim)
+                R_vec = np.append(R_vec, [sat.R_weight_range] * (meas_dim - sat.bearing_dim))
 
                 # Calculate Jacobian matrix H for combined state (still just one satellite H)
-                H = combined_state_H(sat, meas_dim, state_dim, i, k)
+                H = combined_state_H(sat, meas_dim, state_dim)
 
                 # Calculate h
                 h[0 : sat.bearing_dim] = sat.h_landmark(sat.x_p[0:3])
-                for j in range(sat.bearing_dim, meas_dim):
-                    h[j] = sat.h_inter_range(i + 1, j, sat.x_p[0:3])
-                    R_vec = np.append(R_vec, sat.R_weight_range)
+                h[sat.bearing_dim : meas_dim] = sat.h_inter_range(sat.x_p[0:3])
 
+                # TODO: Change these all to lists to speed up appending
                 y_m[0 : sat.bearing_dim] = sat.measure_z_landmark()
                 y_m[sat.bearing_dim : meas_dim] = sat.measure_z_range(sats_copy)
 
                 # Append vectors and matrices to combined form
                 comb_y_m = np.append(comb_y_m, y_m, axis=0)
                 comb_h = np.append(comb_h, h, axis=0)
-                if k == 0:
+                if sat.id == 0:
                     comb_H = H
                 else:
                     comb_H = np.append(comb_H, H, axis=0)
@@ -216,6 +218,7 @@ def run_simulation(args):
                 .reshape(-1)
             )
             pos_error[trial, i, :] = filter_position[trial, i, :] - comb_curr_pos
+            # print(pos_error[trial, i, 0])
 
             # # Sanity check that Cov - FIM is positive definite (Should always be true)
             fpost_sanity_check(f_post, sat, args.verbose, state_dim)
@@ -316,6 +319,18 @@ if __name__ == "__main__":
         generate_satellites_yaml(filename="config/sat_autogen.yaml", n_sats=args.n_sats)
 
     args.landmark_objects = landmark_objects
+
+    # Check if random_yaml not set that the number of satellites specified is less than or equal to the number of satellites in the yaml config file
+    if not args.random_yaml:
+        with open("config/config.yaml", "r") as file:
+            config = yaml.safe_load(file)
+
+            if len(config["satellites"]) < args.n_sats:
+                raise ValueError(
+                    """Number of satellites specified is greater than the number of satellites in the yaml file. 
+                    Add --random_yaml flag to generate random satellite configuration for the provided number of 
+                    satellites or create custom satellites in config/config.yaml"""
+                )
 
     if args.run_all:
         for i in range(1, args.n_sats + 1):

sat/construct_jacobian.py
@@ -8,7 +8,7 @@ import numpy as np
 # pylint: disable=invalid-name
 from sat.core import satellite
 
-def combined_state_H(sat: satellite,  meas_dim: int, state_dim: int, timestep: int, k: int) -> np.ndarray:
+def combined_state_H(sat: satellite,  meas_dim: int, state_dim: int) -> np.ndarray:
 
     """
     Calculate the Jacobian matrix for a single satellite using the combined measurement model.
@@ -17,28 +17,19 @@ def combined_state_H(sat: satellite,  meas_dim: int, state_dim: int, timestep: i
         sat (satellite): The satellite instance.
         meas_dim (int): The dimension of the measurement vector.
         state_dim (int): The dimension of the state vector.
-        timestep (int): The current timestep.
-        k (int): The current satellite index.
 
     Returns:
         np.ndarray: The Jacobian matrix for the satellite.
     """
 
     H = np.zeros((meas_dim, state_dim * sat.n_sats))
-    H[0 : sat.bearing_dim, k * state_dim : (k + 1) * state_dim] = (
+    H[0 : sat.bearing_dim, sat.id * state_dim : (sat.id + 1) * state_dim] = (
         sat.H_landmark(sat.x_p)
     )
 
-    opp_sat_id = 0
-    for j in range(sat.bearing_dim, meas_dim):
-        dist = sat.H_inter_range(timestep + 1, j, sat.x_p)
-        H[j, sat.id * state_dim : (sat.id + 1) * state_dim] = dist
-
-        if opp_sat_id == sat.id:
-            opp_sat_id += 1
-            H[j, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -dist
-        else:
-            H[j, opp_sat_id * state_dim : (opp_sat_id + 1) * state_dim] = -dist
-            opp_sat_id += 1
+    dist_J = sat.H_inter_range(sat.x_p)
+    H[sat.bearing_dim:meas_dim,sat.id * state_dim : (sat.id + 1) * state_dim] = dist_J
+    for i , other_sat in enumerate(sat.curr_visible_sats):
+        H[sat.bearing_dim + i : sat.bearing_dim + i + 1, other_sat.id * state_dim : (other_sat.id + 1) * state_dim] = -dist_J[i, :]
 
     return H

sat/core.py
@@ -1,9 +1,11 @@
+from typing import List
+
 import jax.numpy as jnp
 import jax
 import math
 import numpy as np
 
-
+from landmarks.landmark import landmark
 from utils.math_utils import R_X, R_Z
 
 # Constants
@@ -67,6 +69,7 @@ class satellite:
         self.ignore_earth = ignore_earth
         
         # Initialize the measurement vector with noise
+        np.random.seed(123)
         self.x_m = self.x_0# + np.array([1,0,0,0,0,0]) # Initialize the measurement vector exactly the same as the initial state vector
         x_m_init_noise = np.random.normal(loc=0,scale=math.sqrt(self.R_weight_bearing),size=int(self.dim/2))
         # Normalize the noise vector
@@ -86,19 +89,24 @@ class satellite:
         self.curr_visible_landmarks = []
         self.HEIGHT = 550
 
-
-    def visible_landmarks_list(self, x) -> int:
-        norm_vec_sat = x[0:3]/jnp.linalg.norm(x[0:3])
-        r_earth = x[0:3] - self.HEIGHT*norm_vec_sat
-        theta_t = (self.HEIGHT/jnp.linalg.norm(r_earth))*(self.camera_fov/2)
-
+    ### Visibility functions for landmarks and satellites ###
+    def visible_landmarks_list(self) -> List[landmark]:
+        # TODO: can be made faster by inferring that landmarks are more likely to be visible if they were visible in the previous timestep
         self.curr_visible_landmarks = []
-
         for landmark in self.landmarks:
-            if self.landmark_visible(landmark.pos, r_earth, theta_t):
+            if self.is_visible_ellipse(self.curr_pos, landmark.pos): # TODO: Consider ignoring the earth or not the speed up of ignoring is crazy.
                 self.curr_visible_landmarks.append(landmark)
 
         return self.curr_visible_landmarks
+    
+
+    def visible_sats_list(self, sats: List["satellite"]) -> List["satellite"]:
+        self.curr_visible_sats = []
+        for sat in sats:
+            if sat.id != self.id:
+                if self.ignore_earth or self.is_visible_ellipse(self.curr_pos, sat.curr_pos):
+                    self.curr_visible_sats.append(sat)
+        return self.curr_visible_sats
 
 
     def h_landmark(self, x):
@@ -114,20 +122,21 @@ class satellite:
         # Use jax to autodifferentiate
         jac = jax.jacobian(self.h_landmark)(x)
         return jac
+    
 
-    def h_inter_range(self, timestep, j, x):
-        sat_id = j-self.bearing_dim # j is the range measurement index starting from 3
-        sat_pos = self.other_sats_pos[timestep,:,sat_id] # sat_id here refers to the first other satellite. Indexing starts again from 0
-        if self.is_visible_ellipse(x[0:3], sat_pos) or self.ignore_earth:
-            return jnp.linalg.norm(x[0:3] - sat_pos) 
-        else:
-            return jnp.linalg.norm(0)
+    def h_inter_range(self, x):
+        h = jnp.zeros((len(self.curr_visible_sats)))
+        for i, sat in enumerate(self.curr_visible_sats):
+            h= h.at[i].set(jnp.linalg.norm(x[0:3] - sat.curr_pos))
+        
+        return h
 
 
-    def H_inter_range(self, i, j, x):
-        jac = jax.jacobian(self.h_inter_range, argnums=2)(i, j, x)
+    def H_inter_range(self, x):
+        jac = jax.jacobian(self.h_inter_range, argnums=0)(x)
         return jac
     
+    # TODO: Simplify the bearing functions
     def h_sat_bearing(self, timestep, j, x):
         sat_id = j-self.bearing_dim # j is the bearing measurement index starting from 3
         sat_pos = self.other_sats_pos[timestep,:,sat_id] # sat_id here refers to the first other satellite. Indexing starts again from 0
@@ -142,26 +151,20 @@ class satellite:
 
 
     def measure_z_range(self, sats: list) -> np.ndarray:
-        z = np.empty((0))
-        for sat in sats:
-            if sat.id != self.id:
-                if self.is_visible_ellipse(self.curr_pos, sat.curr_pos) or self.ignore_earth: # If the earth is not in the way, we can measure the range. Or if earth is ignored
-                    if self.verbose:
-                        print(f"Satellite {self.id} can see satellite {sat.id}")
-                    noise = np.random.normal(loc=0,scale=math.sqrt(self.R_weight_range),size=(1))
-                    d = np.array([np.linalg.norm(self.curr_pos - sat.curr_pos)]) + noise
-                    z = np.append(z,d,axis=0)
+        z = np.array([])
+        for sat in self.curr_visible_sats:
+            if self.verbose:
+                print(f"Satellite {self.id} can see satellite {sat.id}")
+            noise = np.random.normal(loc=0,scale=math.sqrt(self.R_weight_range),size=(1))
+            d = np.array([np.linalg.norm(self.curr_pos - sat.curr_pos)]) + noise
+            z = np.append(z,d,axis=0)
 
-                else: # If the earth is in the way , we set the value to nan so it does not feature in the objective function
-                    z = np.append(z,np.array([0]),axis=0)
         return z
     
 
     def measure_z_landmark(self) -> np.ndarray:
         z_l = np.zeros((len(self.curr_visible_landmarks)*3))
-
         if self.camera_exists:
-
             for i, landmark in enumerate(self.curr_visible_landmarks):
                 if self.verbose:
                     print(f"Satellite {self.id} can see landmark {landmark.name} visible")
@@ -209,18 +212,18 @@ class satellite:
             return False
         
         return True
-    
 
-    def landmark_visible(self, landmark_pos, r_earth, theta_t) -> bool:
-        # Check if a landmark can be seen from the current position of the satellite when taking a picture
-        # Assuming that the camera is pointing straight down
-        # FOV is about 60 degrees 
-        # ASSUMPTION: The satellite is flying at an altitude of around 550km consistently
-        # Otherwise we need a separate way to determine the altitude of the satellite 
+    ## SIMPLIFIED INTERCEPTOR FOR ROUND EARTH ASSUMPTION ##
+    # def landmark_visible(self, landmark_pos, r_earth, theta_t) -> bool:
+    #     # Check if a landmark can be seen from the current position of the satellite when taking a picture
+    #     # Assuming that the camera is pointing straight down
+    #     # FOV is about 60 degrees 
+    #     # ASSUMPTION: The satellite is flying at an altitude of around 550km consistently
+    #     # Otherwise we need a separate way to determine the altitude of the satellite 
         
-        theta_l = jnp.rad2deg(jnp.arccos(jnp.dot(r_earth,landmark_pos)/(jnp.linalg.norm(r_earth)*jnp.linalg.norm(landmark_pos))))
+    #     theta_l = jnp.rad2deg(jnp.arccos(jnp.dot(r_earth,landmark_pos)/(jnp.linalg.norm(r_earth)*jnp.linalg.norm(landmark_pos))))
         
-        if theta_l < theta_t:
-            return True
-        else:
-            return False
+    #     if theta_l < theta_t:
+    #         return True
+    #     else:
+    #         return False